b82b815505f006bcbe8e46d89abbcb263eb86378
Bryan Arévalo <bryantello2010@hotmail.com>
2023-12-23 14:34:12 -0600

    Added http handler functions

--- ripFramework/httpHandlers.go
+++ ripFramework/httpHandlers.go
@@ -1,7 +1,21 @@
 package ripframework
 
-func (f *Framework) Get(uri string) {
+import "ripframework/ripFramework/constants"
 
+func (f *Framework) setRoute(verb string, uri string, handler handleFunction) {}
+
+func (f *Framework) Get(uri string, handler handleFunction) {
+	f.setRoute(constants.GET, uri, handler)
 }
 
-func (f *Framework) Post(uri string) {}
+func (f *Framework) Post(uri string, handler handleFunction) {
+	f.setRoute(constants.POST, uri, handler)
+}
+
+func (f *Framework) Put(uri string, handler handleFunction) {
+	f.setRoute(constants.PUT, uri, handler)
+}
+
+func (f *Framework) Delete(uri string, handler handleFunction) {
+	f.setRoute(constants.DELETE, uri, handler)
+}
