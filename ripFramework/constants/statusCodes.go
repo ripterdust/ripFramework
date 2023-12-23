0a3b518df1c266643680e6a73c2ec3d42fe33a85
Bryan Arévalo <bryantello2010@hotmail.com>
2023-12-23 14:30:48 -0600

    Added status codes

--- ripFramework/constants/statusCodes.go
+++ ripFramework/constants/statusCodes.go
@@ -1,1 +1,9 @@
 package constants
+
+const (
+	HTTP_SUCCESS               = 200
+	HTTP_CREATED               = 201
+	HTTP_INTERNAL_SERVER_ERROR = 500
+	HTTP_NOT_FOUND             = 404
+	HTTP_NOT_AUTHORIZED        = 403
+)
